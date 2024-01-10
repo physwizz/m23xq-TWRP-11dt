c7c1674b2076cff400ffc1a974ed27413f807e61
physwizz <pollockg86@gmail.com>
2024-01-10 12:02:43 +1100

    first

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_m23xq.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_m23xq-user \
+    twrp_m23xq-userdebug \
+    twrp_m23xq-eng
